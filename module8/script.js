e2eecea251f00ef9cffba25f39b7a6189ecd42bc
Balaji Koneti <balajishiva2580@gmail.com>
2024-07-22 09:55:27 -0700

    Delete module8 directory

--- module8/script.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener('DOMContentLoaded', () => {
-    const taskList = document.getElementById('taskList');
-    
-    taskList.addEventListener('click', (event) => {
-        if (event.target && event.target.matches('li.task')) {
-            event.target.classList.toggle('selected');
-        }
-    });
-});
